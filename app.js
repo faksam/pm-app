861650ddc7f5f679a9e5f041274d68af220eee5d
Samuel Fakunle <fakunlesamuel@gmail.com>
2021-03-22 02:34:30 -0500

    remove import statement

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const path = require('path');
 const express = require('express');
-import * as dotenv from 'dotenv';
 
 const app = express();
-dotenv.config();
 
 app.use(express.static(`${__dirname}/dist`));
 
